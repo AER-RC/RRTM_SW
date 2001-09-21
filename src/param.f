7a45673e11e0bccc9f8487a2bb116657f3d75f15
jdelamer <jdelamer>
2001-09-21 13:24:31 +0000

    Added mxstr parameter

--- src/param.f
+++ src/param.f
@@ -1,6 +1,7 @@
 	parameter (mxlay = 203, nbands = 29)
 	parameter (ib1 = 16, ib2 = 29)
         parameter (mg = 16)
+	parameter (mxstr = 16)
 
       COMMON /BANDS/     WAVENUM1(IB1:IB2),
      &                   WAVENUM2(IB1:IB2),
