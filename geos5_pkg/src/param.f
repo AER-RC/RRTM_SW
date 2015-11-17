9284459a7d5e466de7a44d98b5149a5091f41537
kcadyper <kcadyper>
2015-11-17 21:52:11 +0000

    This package inlcludes teh code delivered to KAUST (original and updates), which was delivered to GEOS5 team.

---
+++ geos5_pkg/src/param.f
@@ -0,0 +1,13 @@
+C     path:      $Source$
+C     author:    $Author$
+C     revision:  $Revision$
+C     created:   $Date$
+
+	parameter (mxlay = 203, nbands = 29)
+	parameter (ib1 = 16, ib2 = 29)
+        parameter (mg = 16)
+	parameter (mxstr = 16)
+
+      COMMON /BANDS/     WAVENUM1(IB1:IB2),
+     &                   WAVENUM2(IB1:IB2),
+     &                   DELWAVE(IB1:IB2)
